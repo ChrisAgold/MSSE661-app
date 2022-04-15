05cfc79b5f89059cc6291caa41f3c6166a381c25
Chris Agold <christophersagold@gmail.com>
2022-04-15 15:43:00 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+
+const app = express()
+import dotenv from 'dotenv';
+dotenv.config();
+
+// db & authenticateUser
+import connectDB from './db/connect.js';
+
+// ROUTERS
+import authRouter from "./routes/authRoutes.js";
+
+// MIDDLEWARE
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.use(express.json())
+
+// ROUTE
+app.get('/', (req, res) => {
+    throw new Error('error')
+    res.send('Welcome!');
+})
+
+app.use('/api/v1/auth',authRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
